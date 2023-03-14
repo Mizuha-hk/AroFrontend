46b3ca4c09f235634154b0450d1e74fa7a37c71a
Mizuha-hk <hk.haru0803@gmail.com>
2023-03-14 21:25:42 +0900

    [add] Services class

--- allo-frontend/Program.cs
+++ allo-frontend/Program.cs
@@ -9,7 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://app-backend-debug.azurewebsites.net/api/") });
+builder.Services.AddScoped<Services>();
 builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
